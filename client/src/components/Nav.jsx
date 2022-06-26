b376f0906682bef979a7568b174db84d6163ddf7
Alvin Rumbaoa <alvinrumbaoa@gmail.com>
2022-06-25 22:30:20 -0700

    installed frontend

---
+++ client/src/components/Nav.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Nav = () => {
+	return (
+		<div className="bg-sky-600 hover:bg-slate-200">
+				<ul>
+					<li>Home</li>
+					<li>My Notes</li>
+					<li>About</li>
+				</ul>
+		</div>
+	)
+}
+
+export default Nav
---
+++ client/src/styles/theme.js
@@ -0,0 +1,26 @@
+import { extendTheme } from '@chakra-ui/react'
+import "@fontsource/inter"
+import { createBreakpoints } from '@chakra-ui/theme-tools'
+import "@fontsource/khand"
+
+// 2. Update the breakpoints as key-value pairs
+const breakpoints = createBreakpoints({
+  sm: '390px',
+  md: '700px',
+  lg: '920px',
+  xl: '1200px',
+  '2xl': '1536px',
+})
+
+const theme = extendTheme({
+
+  breakpoints,
+  fonts: {
+    heading: '"Inter", sans-serif; ',
+    body: '"Khand", sans-serif; ',
+  
+  },
+ 
+})
+
+export default theme;
