617e57e8c212422631cb9843bd87189505ea78b6
Alvin Rumbaoa <alvinrumbaoa@gmail.com>
2022-06-28 01:36:53 -0700

    react router with axios

--- client/src/components/CreateNote.js
+++ client/src/components/CreateNote.js
@@ -1,10 +1,21 @@
-import React from 'react'
-
+import React, {useState} from 'react'
+import { Box , Heading, Textarea, Button, Text}  from  '@chakra-ui/react'
 const CreateNote = () => {
+
+	const [message, setMessage] = useState("");
+
+	const handleInput = (e) => {
+		let inputValue = e.target.value
+		setMessage(e);
+	}
+
+
 	return (
-		<div>
-			<h1>Create</h1>
-		</div>
+		<Box background="gray.100" p={10} w="100%">
+			<Heading color="gray.800">Create</Heading>
+			<Textarea mt={2} placeholder='Here is a sample placeholder' />
+			<Button mt={2} type="submit">Submit</Button>
+		</Box>
 	)
 }
 
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,26 @@
-import React from 'react'
-import { Box , Heading, Text}  from  '@chakra-ui/react'
-const Home = () => {
+import React,{useState} from 'react'
+import { Box , Heading, Button, Flex, Text}  from  '@chakra-ui/react'
+import{ Link} from 'react-router-dom'
+import { useClipboard } from '@chakra-ui/react'
+
+const Home = (props) => {
+	const [value, setValue] = useState("");
+	const [message, setMessage] = useState("");
+	const { hasCopied, onCopy } = useClipboard(value)
+
+
+
+
 	return (
-		<div>
-			<h1>Home Screen</h1>
-		</div>
+		<Box background="gray.100" p={10} w="100%">
+			<Heading textAlign="center" p={10} >Welcome</Heading>
+			<Flex justify="space-between">
+			
+			<Button colorScheme="blackAlpha"> <Link to="/create">Add New Note</Link></Button>
+			</Flex>
+	
+			
+		</Box>
 	)
 }
 
