289aa7ddbb6ea2a338077fe9b2cb8299a637886d
Alvin Rumbaoa <alvinrumbaoa@gmail.com>
2022-06-29 07:46:52 -0700

    navigation fixed

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -3,7 +3,7 @@ import { Box , Text, Heading, Hide} from '@chakra-ui/react'
 import {Link, Outlet} from 'react-router-dom';
 const Nav = () => {
 	return (
-		<Box p={2} bg='gray.600' color='white' h="100vh" justify='center' w="20%"> 
+		<Box p={2} bg='gray.600' color='white'  justify='center' w="20%"> 
 			<Hide below='md'><Heading >Notes App</Heading></Hide>
 
 			<Text mt={2} fontWeight="bold"><Link to="/">Home</Link></Text>
@@ -11,7 +11,7 @@ const Nav = () => {
 			<Text mt={2} fontWeight="bold"><Link to="/create">Create a New Note</Link></Text>
 			<Outlet />
 
-			
+
 		</Box>
 	
 	)
